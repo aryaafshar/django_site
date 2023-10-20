28083611aa19228559e3bbdcf114f93b01c891e7
Qudratullah Afshar <aryaafshar@mail.com>
2023-10-20 10:22:56 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('upload/', views.upload, name='upload'),
     path('admin/', admin.site.urls),
-    '''path('books/', views.book_list, name='book_list'),
-    path('books/upload/', views.upload_book, name='upload_book'),
-    path('books/<int:pk>/', views.delete_book, name='delete_book'),
+    #path('books/', views.book_list, name='book_list'),
+    #path('books/upload/', views.upload_book, name='upload_book'),
+    #path('books/<int:pk>/', views.delete_book, name='delete_book'),
 
-    path('class/books/', views.BookListView.as_view(), name='class_book_list'),
-    path('class/books/upload/', views.UploadBookView.as_view(), name='class_upload_book'), '''
+    #path('class/books/', views.BookListView.as_view(), name='class_book_list'),
+    #path('class/books/upload/', views.UploadBookView.as_view(), name='class_upload_book'), 
 ]
 
 if settings.DEBUG:
